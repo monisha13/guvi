c81ea303a2f83daa236d446e4836b8a8193276ff
monisha13 <47376867+monisha13@users.noreply.github.com>
2019-02-27 01:32:40 -0800

    Create set8.java

---
+++ set8.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int num, count, total = 0;
+
+        
+        Scanner scan = new Scanner(System.in);
+        
+        num = scan.nextInt();
+        
+        scan.close();
+        for(count = 1; count <= num; count++){
+            total = total + count;
+        }
+
+        System.out.println(+total);
+    }
+}
+
+    
+
+	
