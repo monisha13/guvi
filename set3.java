6cf7eb4fb4884f6b4a7fe129c545d1e2304e3936
monisha13 <47376867+monisha13@users.noreply.github.com>
2019-02-27 00:18:32 -0800

    Create set3.java

---
+++ set3.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char ch= ' ';
+	Scanner s=new Scanner(System.in);
+	ch=s.next().charAt(0);
+	int n=(int)ch;
+	if(n>=65 && n<=122)
+	{
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='U'||ch=='O')
+		{
+		System.out.println("Vowels");
+		}
+		else
+		{
+		System.out.println("Consonants");
+		}
+	}
+	else
+	{
+	System.out.println("Invalid");
+	}
+	
+	}
+}
